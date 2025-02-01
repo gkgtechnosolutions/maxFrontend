baba8245c8987607f4c94772cf2444580859b04a
vinayA1996 <126498393+vinayA1996@users.noreply.github.com>
2025-02-01 18:15:15 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,41 +3,44 @@ import { RouterOutlet } from '@angular/router';
 import { AuthModule } from './auth/auth.module';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { BackendhealthService } from './services/backendhealth.service';
-import { MaintainceComponent } from "./maintaince/maintaince.component";
+import { MaintainceComponent } from './maintaince/maintaince.component';
 import { AppConfigService } from './services/app-config.service';
 import { TokenCheckService } from './services/token-check-service.service';
 import { SseServiceService } from './services/sse-service.service';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet, AuthModule, MaintainceComponent]
+  selector: 'app-root',
+  standalone: true,
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, AuthModule, MaintainceComponent],
 })
 export class AppComponent implements OnInit {
   title = 'adminpanel';
   data: any;
 
-  statusMessage :boolean = true;
-   constructor(private backendHealth :BackendhealthService,
-            private configService : AppConfigService,
-            private tokenCheckService: TokenCheckService,
-            private sseService: SseServiceService, private http: HttpClient
-){}
-
+  statusMessage: boolean = true;
+  constructor(
+    private backendHealth: BackendhealthService,
+    private configService: AppConfigService,
+    private tokenCheckService: TokenCheckService,
+    private sseService: SseServiceService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
-    this.sseService.getServerSentEvent('https://0djs0mh1-8080.inc1.devtunnels.ms/sse/subscribe')
-    .subscribe({
-      next: (message) => {
-        console.log('Received:', message);
-        this.fetchData();
-      },
-      error: (err) => console.error('Error:', err)
-    });
+    this.sseService
+      .getServerSentEvent(
+        'http://localhost:8080/approveOperation/sse/subscribe'
+      )
+      .subscribe({
+        next: (message) => {
+          console.log('Received:', message);
+        },
+        error: (err) => console.error('Error:', err),
+      });
 
     //==============================================
     // this.tokenCheckService.startTokenCheck();
@@ -52,25 +55,22 @@ export class AppComponent implements OnInit {
       (error) => {
         this.backendHealth.checkHealthSec().subscribe(
           (response) => {
-        //need to false
-      
-          
-        
-          },(error)=>{
+            //need to false
+          },
+          (error) => {
             console.log(error);
-              this.statusMessage = false;
-          });
-        
+            this.statusMessage = false;
+          }
+        );
       }
     );
-
-    
   }
   // ngOnDestroy(): void {
   //   this.tokenCheckService.stopTokenCheck();
   // }
   fetchData() {
-    this.http.get('https://0djs0mh1-8080.inc1.devtunnels.ms/api/data')
-      .subscribe((response) => this.data = response);
+    this.http
+      .get('https://0djs0mh1-8080.inc1.devtunnels.ms/api/data')
+      .subscribe((response) => (this.data = response));
   }
 }
