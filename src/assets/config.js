ed510f1bfd86e971b81492a0e1f8072b7075511f
PratikBhosale01 <120355158+PratikBhosale01@users.noreply.github.com>
2025-02-03 11:50:40 +0530

    config for pull

--- src/assets/config.js
+++ src/assets/config.js
@@ -6,7 +6,7 @@ var appConfig = {
 
   // mainbackendUrl: "http://agserver-61043.portmap.host:61043",
 
-  mainbackendUrl: "http://13.200.63.62:8080",
+  // mainbackendUrl: "http://13.200.63.62:8080",
 
   // mainbackendUrl:"http://0djs0mh1-8080.inc1.devtunnels.ms",
 
